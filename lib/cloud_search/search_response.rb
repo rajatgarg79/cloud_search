250b9419a1878d732e4b6396a3070455e27f23f3
Daniel Konishi <dkonishi@gmail.com>
2012-10-18 15:59:07 -0300

    Refactoring

--- lib/cloud_search/search_response.rb
+++ lib/cloud_search/search_response.rb
@@ -4,25 +4,25 @@ module CloudSearch
     attr_accessor :http_code
 
     def results
-      (_hits and _hits['hit']) or []
+      _hits["hit"] || []
     end
 
     def hits
-      (_hits and _hits['found']) or 0
+      _hits["found"] || 0
     end
 
     def found?
-      hits >= 1
+      hits > 0
     end
 
     def body=(body)
-      @body = body
+      @body = body || {}
       calculate_pages
       @body
     end
 
     def items_per_page
-      @items_per_page or 10
+      @items_per_page || 10
     end
 
     alias :page_size :items_per_page
@@ -38,18 +38,19 @@ module CloudSearch
     end
 
     private
+
     def calculate_pages
       num_full_pages = hits / items_per_page
       @total_pages   = hits % items_per_page > 0 ? num_full_pages + 1 : num_full_pages
       @total_pages   = 1 if @total_pages == 0
 
-      start = (_hits and _hits["start"]) || 0
+      start = _hits["start"] || 0
       @current_page = (start / items_per_page) + 1
       @current_page = @total_pages if @current_page > @total_pages
     end
 
     def _hits
-      body and body['hits']
+      body["hits"] || {}
     end
   end
 end
