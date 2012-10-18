de5bf36e9d4e65920aaee22992b719a1eeb6f2c4
Cássio Marques <cassio.marques@baby.com.br>
2012-10-18 15:36:05 -0300

    Removes unused context on searcher_spec.rb

--- spec/cloud_search/searcher_spec.rb
+++ spec/cloud_search/searcher_spec.rb
@@ -140,33 +140,31 @@ describe CloudSearch::Searcher do
   describe "#search" do
     before do
       subject
-        .with_fields(:actor, :director, :title, :year, :text_relevance)
-        .with_query("star wars")
+      .with_fields(:actor, :director, :title, :year, :text_relevance)
+      .with_query("star wars")
     end
 
     around { |example| VCR.use_cassette "search/request/full", &example }
 
-    context "given valid parameters" do
-      it "returns http 200 code" do
-        resp = subject.search
-        resp.http_code.should == 200
-      end
+    it "returns http 200 code" do
+      resp = subject.search
+      resp.http_code.should == 200
+    end
 
-      it "has found results" do
-        resp = subject.search
-        resp.should be_found
-      end
+    it "has found results" do
+      resp = subject.search
+      resp.should be_found
+    end
 
-      it "returns number of hits" do
-        resp = subject.search
-        expect(resp.hits).to be == 7
-      end
+    it "returns number of hits" do
+      resp = subject.search
+      expect(resp.hits).to be == 7
+    end
 
-      it "returns Episode II" do
-        resp = subject.search
-        resp.results.inject([]){|acc, i| acc << i['data']['title']}.flatten
-        .should include "Star Wars: Episode II - Attack of the Clones"
-      end
+    it "returns Episode II" do
+      resp = subject.search
+      resp.results.inject([]){|acc, i| acc << i['data']['title']}.flatten
+      .should include "Star Wars: Episode II - Attack of the Clones"
     end
   end
 end
