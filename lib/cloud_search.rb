487f93b37ceeba4c6eb0a5fd3db12002a0e52d6d
pahagon <paulo.ahagon@gmail.com>
2012-11-29 11:13:42 -0200

    Update autload

--- lib/cloud_search.rb
+++ lib/cloud_search.rb
@@ -4,12 +4,13 @@ require "json"
 require "cloud_search/version"
 
 module CloudSearch
-  autoload :Config,          "cloud_search/config"
-  autoload :Searcher,        "cloud_search/searcher"
-  autoload :SearchResponse,  "cloud_search/search_response"
-  autoload :Indexer,         "cloud_search/indexer"
-  autoload :Document,        "cloud_search/document"
-  autoload :InvalidDocument, "cloud_search/invalid_document"
+  autoload :Config,                "cloud_search/config"
+  autoload :ConfigurationChecking, "cloud_search/config"
+  autoload :Searcher,              "cloud_search/searcher"
+  autoload :SearchResponse,        "cloud_search/search_response"
+  autoload :Indexer,               "cloud_search/indexer"
+  autoload :Document,              "cloud_search/document"
+  autoload :InvalidDocument,       "cloud_search/invalid_document"
 
   def self.config
     Config.instance
